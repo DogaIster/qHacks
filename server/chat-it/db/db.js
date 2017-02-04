fb769cf3dd3c4593aea660267daeac0c0a5ead32
omotherhen <ngu.henry@hotmail.com>
2017-02-04 06:45:07 -0500

    implement postItinerary

--- server/chat-it/db/db.js
+++ server/chat-it/db/db.js
@@ -1,38 +1,117 @@
 const connect = require('camo').connect;
-var Document = require('camo').Document;
-
+const Document = require('camo').Document;
+const EmbeddedDocument = require('camo').EmbeddedDocument;
+const uri = 'nedb:///data/chat-it';
 let db;
-var uri = 'nedb:///data/chat-it';
 
-class Dog extends Document {
+
+class User extends Document {
 	constructor() {
 		super();
+		this.username = String;
+		this.password = String;
+	}
 
-		this.name = String;
-		this.breed = String;
+	static verify(user, password) {
+		if (user.password === password)
+			return;
+
+		return 'Invalid login';
+	}
+	static async get(data) {
+		let user = await this.find({
+			username: data.username
+		});
+		return this.verify(user, data.password);
+	}
+
+	static async add(data) {
+		//check if user exists yet
+		let u = this.find({
+			username: data.username
+		});
+		if (u) {
+			return {
+				error: 'User already exists'
+			};
+		}
+
+		let user = User.create(data);
+		try {
+			await user.save();
+			return;
+		} catch (e) {
+			return console.log(e);
+		}
+	}
+}
+
+class Entry extends EmbeddedDocument {
+	constructor() {
+		super();
+		this.location = String;
+		this.activity = String;
+		this.time = String;
+		this.duration = Number;
+	}
+}
+
+
+class Itin extends Document {
+	constructor() {
+		super();
+		this.user = String;
+		this.location = String;
+		this.dateTo = String;
+		this.dateFrom = String;
+		this.data = [
+			[Entry]
+		];
+	}
+
+
+	static async add(data) {
+		let deleted = await this.deleteMany({});
+		console.log(`Deleted:${deleted}`);
+		let itin = Itin.create();
+		//itin.user = 'blah';
+		itin.location = data.location;
+		itin.dateTo = data.dateTo;
+		itin.dateFrom = data.dateFrom;
+		itin.data = [];
+
+		for (let day of data.data) {
+			let hour = [];
+			//console.log(day)
+			for (let entry of day) {
+				//console.log(entry);
+				let entryObj = Entry.create(entry);
+				hour.push(entryObj);
+			}
+			itin.data.push(hour);
+		}
+
+		try {
+			await itin.save();
+		} catch (e) {
+			console.log(e);
+			return;
+		}
+		console.log('itinerary successfully saved!');
+	}
+
+	static async get() {
+		return await this.find({});
 	}
 }
 
 
 connect(uri).then(function (database) {
 	db = database;
-	console.log('Connected!');
-	var lassie = Dog.create({
-		name: 'Lassie',
-		breed: 'Collie'
-	});
-
-	lassie.save().then(function (l) {
-		console.log(l._id);
-
-
-		Dog.findOne({
-			name: 'Lassie'
-		}).then(function (l) {
-			console.log('Got Lassie!');
-			console.log('Her unique ID is', l._id);
-		});
-
-	});
-
+	console.log('Connected to db!');
 });
+
+module.exports = {
+	Itin,
+	User,
+};
