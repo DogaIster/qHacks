bc2bf130aaba5c215b1293782bb23b6042ff1734
omotherhen <ngu.henry@hotmail.com>
2017-02-05 04:31:11 -0500

    added register

--- new_UI/Angular2_CLI_Full_Project/src/app/pages/register.component.ts
+++ new_UI/Angular2_CLI_Full_Project/src/app/pages/register.component.ts
@@ -9,29 +9,44 @@ import { User } from './user';
 import * as $ from 'jquery';
 
 @Component({
-  selector: 'register',
-  templateUrl: 'register.component.html'
+    selector: 'register',
+    templateUrl: 'register.component.html'
 })
-export class RegisterComponent implements AfterViewInit{
+export class RegisterComponent implements AfterViewInit {
 
-  username1: any;
-  password1: any;
-  email1: any;
-  phone_number1: any;
+    username1: any;
+    password1: any;
+    email1: any;
+    phone_number1: any;
 
-  user = new User(this.username1, this.password1, this.email1, this.phone_number1);
+    user = new User(this.username1, this.password1, this.email1, this.phone_number1);
 
-  constructor(){}
+    constructor() { }
 
-  ngAfterViewInit(){
-    console.log('his')
-    let login = {
-      name: 'henry',
-      password: 'nguyen'
+    ngAfterViewInit() {
+        let register = {
+          username: '',
+          password: '',
+          email: '',
+          phoneNumber: 0,
+        }
+        $("#registerButton").click(() => {
+          register.username = $('#username').val();
+          register.password = $('#password').val();
+          register.email = $('#email').val();
+          register.phoneNumber = +$('#phoneNumber').val();
+          console.log(register);
+          $.ajax({
+              url: 'http://50.112.200.45/register',
+              type: "POST",
+              async: false,
+              data: JSON.stringify(register),
+              contentType: "application/json",
+              dataType: "json",
+              success: function(data) {
+                console.log('success!', data);
+              }
+          });
+        });
     }
-    $.post('http://50.112.200.45/login', login, (data)=>{
-      console.log('Login success:', data);
-    });
-  }
-
 }
